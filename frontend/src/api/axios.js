6f0c4b9616de0ea6fef0e1ba7b25894ffb24800e
Joseph Cardoza <80924062+JosephCardoza03@users.noreply.github.com>
2026-04-21 20:56:54 -0500

    changed wrong directory

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "https://becausewecare.onrender.com/auth",
+    baseURL: "http://localhost:5003/auth",
     headers: {
         "Content-Type": "application/json"
     }   
